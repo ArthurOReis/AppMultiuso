3b899a5e75621eca2dbac6908d58e45f017bd0ae
ARTHUR OLIVEIRA DOS REIS <arthur_o_reis@estudante.sesisenai.org.br>
2023-04-17 13:34:28 -0300

    Criando dificuldade e Activity Jogo

--- app/src/main/java/com/example/projetopager/Dificuldade.java
+++ app/src/main/java/com/example/projetopager/Dificuldade.java
@@ -2,13 +2,35 @@ package com.example.projetopager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dificuldade extends AppCompatActivity {
-
+    static int dificuldade;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dificuldade);
+        getSupportActionBar().hide();
     }
+
+    public void facil (View v){
+        dificuldade = 1;
+        Intent i = new Intent(this, Jogo.class);
+        startActivity(i);
+    }
+
+    public void medio (View v){
+        dificuldade = 2;
+        Intent i = new Intent(this, Jogo.class);
+        startActivity(i);
+    }
+
+    public void dificil (View v){
+        dificuldade = 3;
+        Intent i = new Intent(this, Jogo.class);
+        startActivity(i);
+    }
+
 }
